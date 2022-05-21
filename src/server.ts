6a2d5fafe730fe42488a2ff5c4a570c6d0f9eafa
Hidemitsu <hidemitsu.hashimoto@gmail.com>
2022-05-21 18:24:43 -0300

    feat: add PORT env

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
 
+const PORT = 3333
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen('3333', () => {
-  console.log('Server running in port: 3333')
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running in port: ${process.env.PORT || PORT}`)
 })
